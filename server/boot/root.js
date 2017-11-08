1579f882e0fffd295975ea6919d1fdb5940dc51d
Daniel <Daniel-16-6-96@hotmail.com>
2017-11-08 10:43:50 +0100

    Hasta verificar el email y acceder al enlace y acceder con tu cuenta de correo electronico y contraseña funciona y el log out tambien falta, el cambiar contraseña

--- server/boot/root.js
+++ server/boot/root.js
@@ -9,6 +9,7 @@ var dsConfig = require('../datasources.local.js');
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
+  var User = server.models.Usuario;
   //login page
   router.get('/', function(req, res) {
     var credentials = dsConfig.myEmailDataSource.transports[0].auth;
@@ -21,5 +22,48 @@ module.exports = function(server) {
   //verified
   router.get('/verified', function(req, res) {
     res.render('verified');
+});
+
+ //log a user in
+  router.post('/login', function(req, res) {
+    User.login({
+      email: req.body.email,
+      password: req.body.password
+    }, 'user', function(err, token) {
+      if (err) {
+        if(err.details && err.code === 'LOGIN_FAILED_EMAIL_NOT_VERIFIED'){
+          res.render('reponseToTriggerEmail', {
+            title: 'Login failed',
+            content: err,
+            redirectToEmail: '/api/users/'+ err.details.userId + '/verify',
+            redirectTo: '/',
+            redirectToLinkText: 'Click here',
+            userId: err.details.userId
+          });
+        } else {
+          res.render('response', {
+            title: 'Login failed. Wrong username or password',
+            content: err,
+            redirectTo: '/',
+            redirectToLinkText: 'Please login again',
+          });
+        }
+        return;
+      }
+      res.render('home', {
+        email: req.body.email,
+        accessToken: token.id,
+        redirectUrl: '/api/users/change-password?access_token=' + token.id
+      });
+    });
+});
+
+//log a user out
+  router.get('/logout', function(req, res, next) {
+    if (!req.accessToken) return res.sendStatus(401);
+    User.logout(req.accessToken.id, function(err) {
+      if (err) return next(err);
+      res.redirect('/');
+    });
 });
 };
